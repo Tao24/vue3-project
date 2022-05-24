a6f88ad57b86020016afc99144af3809c5457684
fengweitao <76671597+Tao24@users.noreply.github.com>
2022-05-24 15:16:25 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    HOME
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {}
+})
+</script>
